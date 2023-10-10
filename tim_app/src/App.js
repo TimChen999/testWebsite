055e1c8b70495e12a96b95f023d9db7ec61907a5
Tim Chen <timchenut567@utexas.org>
2023-10-09 19:09:28 -0500

    add text box element of materialUI

--- tim_app/src/App.js
+++ tim_app/src/App.js
@@ -39,11 +39,12 @@ function App() {
   return (
     /*Add custom implementation here (It needs at least one thing)*/
     /*https://react.dev/learn then https://react.dev/learn/describing-the-ui */
-    /*Projects themselves are componenets, but need to make 2 additional subcomponents*/
+    /*Projects themselves are componenets, but need to make 2 additional subcomponents (Use botton and text box as materialUI elements*/
     <div>
       <Image />
       <Image />
       <Button onClick={action} variant="contained">Press to authorize user</Button>
+      <TextField id="outlined-basic" label="Enter text" variant="outlined" />
       <Parent />
     </div>
   );
